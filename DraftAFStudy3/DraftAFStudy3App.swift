cec1d3718d5a3d7e7e5ccd7d94b1cabd35edc5af
Carlos Mbendera <carlosmbendera@gmail.com>
2023-06-13 23:30:34 -0500

    Initial Commit

---
+++ DraftAFStudy3/DraftAFStudy3App.swift
@@ -0,0 +1,17 @@
+//
+//  DraftAFStudy3App.swift
+//  DraftAFStudy3
+//
+//  Created by Carlos Mbendera on 2023-06-13.
+//
+
+import SwiftUI
+
+@main
+struct DraftAFStudy3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
