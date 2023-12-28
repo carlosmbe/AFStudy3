d0d6926e9f5a77b9f31b8ed6005872d75e5ca7d1
Carlos Mbendera <carlosmbendera@gmail.com>
2023-12-28 13:09:37 -0600

    UI Changes, Prompt Groups and Pickers in Surveys

---
+++ DraftAFStudy3/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  DraftAFStudy3
+//
+//  Created by Carlos Mbendera on 2023-12-28.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
